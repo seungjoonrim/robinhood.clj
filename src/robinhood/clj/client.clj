ab4c993bbf8c45ea80a97be7495cd459d1accc7c
Jason Hall <jason13@vt.edu>
2018-08-24 18:47:16 -0400

    Add comments

--- src/robinhood/clj/client.clj
+++ src/robinhood/clj/client.clj
@@ -20,9 +20,36 @@
       (client/get {:accept :json})
       response->body))
 
-(defn quotes [query-params]
+
+;; TODO @HALLJSON it would be cool to get options streaming in real time:
+
+; https://api.robinhood.com/instruments/2f30ec68-bb19-44aa-a289-b50b43c2257c/
+; :tradable_chain_id
+; https://api.robinhood.com/options/chains/6b8ec0c1-3946-4a55-842f-8b9adb2e9b79/
+;
+; https://api.robinhood.com/options/instruments/
+;   ?chain_id=6b8ec0c1-3946-4a55-842f-8b9adb2e9b79
+;   &expiration_dates=2018-09-21
+;   &state=active
+;   &tradability=tradable
+;   &type=call
+
+;; TODO @HALLJSON product value idea;
+;; give me the option to record certain option information into a local database
+;; at arbtitrary intervals so that i can track option values over time
+;;... maybe one day we can operattionalize this and make money yaaaaaay
+
+
+
+; https://api.robinhood.com/midlands/news/EAF/
+; https://api.robinhood.com/instruments/?symbol=MSFT
+; https://api.robinhood.com/midlands/movers/sp500/?direction=up
+; https://api.robinhood.com/midlands/movers/sp500/?direction=down
+
+(defn quotes
+  [query-params]
   (->> query-params
-       (geturl "https://api.robinhood.com/quotes/")
+       (geturl "https://api.robinhood.com/quotes/") ;this / at the end is needed!
        :results))
 
 #_(quotes {:symbols "EAF,MSFT"})
