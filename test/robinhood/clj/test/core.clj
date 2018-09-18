be02dd9bc1edf73fbdec3b73ed9f8d640c237309
Jason Hall <jason13@vt.edu>
2018-09-17 23:32:26 -0400

    Minimum viable test setup

--- test/robinhood/clj/test/core.clj
+++ test/robinhood/clj/test/core.clj
@@ -8,30 +8,26 @@
   (is (not (nil? rr))))
 
 (deftest test-quotes
-  (let [quotes (quotes rr {:symbols "EAF,MSFT"})]
-    (is (= 2 (count quotes)))))
+  (let [test-quotes (quotes rr {:symbols "EAF,MSFT"})]
+    (is (= 2 (count test-quotes)))))
 
 (deftest test-news
-  (let [news' (news rr "MSFT")]
-    (is (= 2 2))))
+  (let [test-news (news rr "MSFT")]
+    (is (< 0 (count test-news)))))
 
 (deftest test-movers
-  (let [movers' (movers rr "up")]
-    (is (= 2 2))))
+  (let [test-movers (movers rr "up")]
+    (is (< 0 (count test-movers)))))
 
 (deftest test-movers
-  (let [movers' (movers rr "down")]
-    (is (= 2 2))))
-
-(deftest test-quotes
-  (let [quotes' (quotes rr {:symbols "EAF,MSFT"})]
-    (is (= 2 2))))
+  (let [test-movers (movers rr "down")]
+    (is (< 0 (count test-movers)))))
 
 (deftest test-instrument
-  (let [instrument' (instrument rr {:symbols "EVC"})]
-    (is (= 2 2))))
+  (let [test-instrument (instrument rr {:symbols "EVC"})]
+    (is (= "EVC" (:symbol test-instrument)))))
 
 (deftest test-instruments
-  (let [instruments' (instruments rr "EVC")]
-    (is (= 2 2))))
-
+  (let [test-instruments (instruments rr "EVC")]
+    (println test-instruments)
+    (is (= 2 (count test-instruments)))))
